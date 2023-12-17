6a301f49950a9aef44b0229628e04dc6666b2206
Alberto Riffaud <alriffaud@google.com>
2023-12-17 09:20:32 -0800

    Modification of shell.h, run_command.c and get_command.c files.

--- get_command.c
+++ get_command.c
@@ -4,41 +4,35 @@
  * get_command - This function gets the name of the command entered when the
  * user enters a direction path.
  * @str: It's a pointer to the direction path.
+ * @only_path: It's a variable with only the path (without the command).
  *
  * Return: It returns a pointer to the name of the command.
  */
 char *get_command(char *str, char *only_path)
 {
-	char *token, *lastToken = NULL;
+	char *token, *lastToken = NULL, *tmp;
 
-	token = strtok(str, "/");
+	tmp = strdup(str);
+	if (tmp == NULL)
+		return (NULL);
+	token = strtok(tmp, "/");
 	while (token != NULL)
 	{
 		if (lastToken != NULL)
 		{
 			strcat(only_path, "/");
-                        strcat(only_path, lastToken);
+			strcat(only_path, lastToken);
 			free(lastToken);
 			lastToken = NULL;
 		}
 		lastToken = strdup(token);
 		if (lastToken == NULL)
+		{
+			free(tmp);
 			return (NULL);
+		}
 		token = strtok(NULL, "/");
 	}
+	free(tmp);
 	return (lastToken);
 }
-
-/*int main(void)
-{
-	char str[] = "/a/b/c/ls";
-	char only_path[100];
-	char *name;
-
-	only_path[0] = '\0';
-	name = get_command(str, only_path);
-	printf("Name: %s\n", name);
-	printf("only_path: %s\n", only_path);
-	free(name);
-	return (0);
-}*/
--- run_command.c
+++ run_command.c
@@ -13,7 +13,7 @@ void run_command(char **args, char *av)
 	char *env[] = {NULL}, *value, *name, *cwd;
 	char **paths;
 	pid_t child_pid;
-	char path[100], only_path[100];
+	char path[200], only_path[100];
 	size_t i;
 
 	if (args == NULL || args[0] == NULL)
@@ -25,10 +25,6 @@ void run_command(char **args, char *av)
 	if ((get_and_find(name) == 1 || get_and_find(args[0]) == 1))
 	{
 		value = _getenv("PATH");
-		/*if (value == NULL)
-		{
-			fprintf(stderr, "Error: Failed to get PATH\n"), free(value);
-			return; }*/
 		paths = str_token(value);
 		child_pid = fork();
 		if (child_pid == -1)
@@ -39,19 +35,16 @@ void run_command(char **args, char *av)
 		{
 			sprintf(path, "./%s", name);
 			execve(path, args, env);
-			/*execve(args[0], args, env);*/
 			for (i = 0; paths[i] != NULL && i < strlen(*paths); i++)
 			{
 				sprintf(path, "%s/%s", paths[i], name);
 				execve(path, args, env); }
 			cwd = malloc(PATH_MAX);
 			get_absolute_path(only_path, cwd);
-			sprintf(path, "%s/%s", cwd , name);
-			execve(path, args, env);
-			free(cwd);
-			perror(args[0]), freeMemory(args), free(name);
-			free_paths_value(paths, value);
-			exit(EXIT_FAILURE); }
+			sprintf(path, "%s/%s", only_path, name);
+			execve(path, args, env), free(cwd);
+			perror(path), freeMemory(args), free(name);
+			free_paths_value(paths, value), exit(EXIT_FAILURE); }
 		else
 			wait(NULL);
 		free_paths_value(paths, value), free(name); }
